755647936add1cdcce621cd5b2c89b146ebd452a
matorop <matorop@unal.edu.co>
2025-05-03 11:55:42 -0500

    test_vf

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,34 +1,28 @@
 # obtain a list of files in the input directory
-import os
+import sys
 
-from .write_count_words import write_count_words
-
-# def read_all_lines():
-#    lines=[]
-#    input_file_list = os.listdir("data/input/")
-#    for filename in input_file_list:
-#        with open(filename,"r",encoding="utf-8") as f:
-#            lines= f.readlines()
-#            all_lines.extend(lines)
-#        return all_lines
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def main():
 
-    input_file_list = os.listdir("data/input/")
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m homework <input_folder> <output_folder>")
+        sys.exit(1)
 
-    # count the frequency of the words in the files in the input directory
-    counter = {}
-    for filename in input_file_list:
-        with open("data/input/" + filename) as f:
-            for l in f:
-                for w in l.split():
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    input_folder = sys.argv[1]
+    output_folder = sys.argv[2]
 
-    # create the directory output/ if it doesn't exist
-    write_count_words(counter)
+    all_lines = read_all_lines(input_folder)
+    all_lines = preprocess_lines(all_lines)
+    words = split_into_words(all_lines)
+    counter = count_words(words)
+    write_word_counts(counter, output_folder)
 
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
